7cc7f254cf30855772f097d57847a68ea9689435
KodywiththeK <jmsds.km@gmail.com>
2025-02-02 03:02:02 +0900

    #feat: Plus, Minus, Trash, LeftChevron 아이콘 추가, 버튼 icon variant 추가

--- packages/design/src/components/ui/icons.tsx
+++ packages/design/src/components/ui/icons.tsx
@@ -3,6 +3,10 @@ import { FaShoppingCart } from "react-icons/fa";
 import { FaStar } from "react-icons/fa6";
 import { FaRegStarHalfStroke } from "react-icons/fa6";
 import { FaRegStar } from "react-icons/fa6";
+import { CiSquarePlus } from "react-icons/ci";
+import { CiSquareMinus } from "react-icons/ci";
+import { PiTrashLight } from "react-icons/pi";
+import { FaAngleLeft } from "react-icons/fa6";
 
 function CartFill({ size = "24", color = "currentColor", className, ...props }: IconBaseProps) {
   return <FaShoppingCart {...props} color={color} size={size} className={className} />;
@@ -20,9 +24,29 @@ function StarStroke({ size = "24", color = "currentColor", className, ...props }
   return <FaRegStar {...props} color={color} size={size} className={className} />;
 }
 
+function Plus({ size = "24", color = "currentColor", className, ...props }: IconBaseProps) {
+  return <CiSquarePlus {...props} color={color} size={size} className={className} />;
+}
+
+function Minus({ size = "24", color = "currentColor", className, ...props }: IconBaseProps) {
+  return <CiSquareMinus {...props} color={color} size={size} className={className} />;
+}
+
+function Trash({ size = "24", color = "currentColor", className, ...props }: IconBaseProps) {
+  return <PiTrashLight {...props} color={color} size={size} className={className} />;
+}
+
+function LeftChevron({ size = "24", color = "currentColor", className, ...props }: IconBaseProps) {
+  return <FaAngleLeft {...props} color={color} size={size} className={className} />;
+}
+
 export const Icons = {
   CartFill,
   StarFill,
   StarHalfFill,
   StarStroke,
+  Plus,
+  Minus,
+  Trash,
+  LeftChevron,
 };
