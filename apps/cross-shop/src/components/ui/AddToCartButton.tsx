541cffe3a72a302272616a47370ca4cd27047458
KodywiththeK <jmsds.km@gmail.com>
2025-02-04 00:34:46 +0900

    #refactor: AddToCart 버튼 삼항연산 제거, 코드 정리

--- apps/cross-shop/src/components/ui/AddToCartButton.tsx
+++ apps/cross-shop/src/components/ui/AddToCartButton.tsx
@@ -25,6 +25,7 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
   const router = useRouter();
   const { toast } = useToast();
   const { cart, addToCart } = useCartStore();
+
   const cartItem = cart.find((item) => item.id === product.id);
   const isSoldOut = product.status === productStatus.SOLD_OUT.value;
   const isCartFull = cart.length >= 3;
@@ -32,11 +33,7 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
   const handleAddToCart = () => {
     if (isCartFull && !cartItem) return;
 
-    const cartProduct: CartItem = {
-      ...product,
-      quantity: 1,
-      isReady: false,
-    };
+    const cartProduct: CartItem = { ...product, quantity: 1, isReady: false };
     addToCart(cartProduct);
     toast({
       title: "장바구니에 담았습니다.",
@@ -49,55 +46,60 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
     });
   };
 
+  if (cartItem) {
+    return (
+      <Box className="flex w-full justify-center gap-2">
+        <UpdateCartItemQuantityButton cartItem={cartItem} />
+      </Box>
+    );
+  }
+
+  if (isSoldOut) {
+    return (
+      <Modal
+        trigger={
+          <ModalTrigger
+            className={cn(buttonVariants({ size: "sm", variant: "secondary" }), "w-full")}
+          >
+            Get Restock Notification
+          </ModalTrigger>
+        }
+        title="🔔 재입고 알림받기"
+        description={"상품이 재입고 되면 알려드릴게요!\n알림을 받으시겠어요?"}
+        confirmText="받을래요"
+        cancelText="괜찮아요"
+        onConfirm={() =>
+          toast({
+            title: "알림신청 완료!",
+            description: `"${product.name}"이 입고되면 빠르게 알려드릴게요!`,
+          })
+        }
+      />
+    );
+  }
+
+  if (isCartFull) {
+    return (
+      <Modal
+        trigger={
+          <ModalTrigger className={cn(buttonVariants({ size: "sm" }), "w-full")}>
+            Add to Cart
+          </ModalTrigger>
+        }
+        title="🛒 장바구니가 가득 찼어요!"
+        description={
+          "상품은 최대 3개까지 담을 수 있어요.\n장바구니의 물건을 삭제하거나 주문해주세요."
+        }
+        confirmText="장바구니로 이동하기"
+        cancelText="그냥 둘러보기"
+        onConfirm={() => router.push(route.cart.path)}
+      />
+    );
+  }
+
   return (
-    <Box>
-      {cartItem ? (
-        <Box className="flex w-full justify-center gap-2">
-          <UpdateCartItemQuantityButton cartItem={cartItem} />
-        </Box>
-      ) : isCartFull ? (
-        <Modal
-          trigger={
-            <ModalTrigger
-              className={cn(buttonVariants({ size: "sm" }), "w-full")}
-              disabled={isSoldOut}
-            >
-              Add to Cart
-            </ModalTrigger>
-          }
-          title="🛒 장바구니가 가득 찼어요!"
-          description={
-            "상품은 최대 세개까지 담을 수 있어요.\n장바구니에 담긴 물건을 삭제 혹은 주문해주세요."
-          }
-          confirmText="장바구니로 이동하기"
-          cancelText="그냥 둘러보기"
-          onConfirm={() => router.push(route.cart.path)}
-        />
-      ) : isSoldOut ? (
-        <Modal
-          trigger={
-            <ModalTrigger
-              className={cn(buttonVariants({ size: "sm", variant: "secondary" }), "w-full")}
-            >
-              Get Restock Notification
-            </ModalTrigger>
-          }
-          title="🔔 재입고 알림받기"
-          description={"상품이 재입고 되면 알려드릴게요!\n알림을 받으시겠어요?"}
-          confirmText="받을래요"
-          cancelText="괜찮아요"
-          onConfirm={() =>
-            toast({
-              title: "알림신청 완료!",
-              description: `다음엔 놓치시지 않도록, "${product.name}"이 입고되면 빠르게 알려드릴게요!`,
-            })
-          }
-        />
-      ) : (
-        <Button className="w-full" size={"sm"} onClick={handleAddToCart}>
-          Add to Cart
-        </Button>
-      )}
-    </Box>
+    <Button className="w-full" size="sm" onClick={handleAddToCart}>
+      Add to Cart
+    </Button>
   );
 }
